4021e1e321c5818156b2e54dfe2c90994801e389
Andre Albuquerque <andrealbuquerque@iMac.home>
2024-09-26 18:29:07 +0100

    first push

---
+++ Makefile
@@ -0,0 +1,43 @@
+NAME = ircserv
+CXXFLAGS = -Wall -Wextra -Werror -std=c++98
+CXX = c++
+COMPRESS = ar rcs
+RM = rm -rf
+
+OBJ_DIR = objects
+SRC_DIR = sources
+HEADER = includes
+S = main
+SRC = $(addprefix $(SRC_DIR)/,$(addsuffix .cpp, $(S)))
+OBJ = $(addprefix $(OBJ_DIR)/,$(notdir $(SRC:.cpp=.o)))
+
+# Colours
+GREEN = \033[1;32m
+ORANGE = \033[1;33m
+RED = \033[1;31m
+CYAN = \033[1;36m
+RESET = \033[0m
+
+all: $(NAME)
+
+$(NAME): $(OBJ_DIR) $(OBJ)
+	@$(CXX) $(CXXFLAGS) $(OBJ) -o $(NAME) -I $(HEADER)
+	@echo "$(CYAN)make$(RESET)   $@ $(GREEN)[OK]$(RESET)"
+
+$(OBJ_DIR):
+	@mkdir -p $@
+
+$(OBJ_DIR)/%.o: $(SRC_DIR)/%.cpp
+	@$(CXX) $(CXXFLAGS) -c $< -o $@ -I $(HEADER)
+
+clean:
+	@$(RM) $(OBJ_DIR)
+	@echo "$(ORANGE)$@$(RESET)  $(NAME) $(GREEN)[OK]$(RESET)"
+
+fclean: clean
+	@$(RM) $(NAME)
+	@echo "$(RED)$@$(RESET) $(NAME) $(GREEN)[OK]$(RESET)"
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ includes/headers.hpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   headers.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: andrealbuquerque <andrealbuquerque@stud    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/04 15:32:24 by andre-da          #+#    #+#             */
+/*   Updated: 2024/09/26 18:27:03 by andrealbuqu      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HEADERS_HPP
+# define HEADERS_HPP
+
+# include <iostream>		// For input and output stream
+# include <string>			// For string class and functions
+# include <cstring>			// For C-style string functions
+# include <cmath>			// For common mathematical functions
+# include <sstream>			// For string stream classes
+# include <fstream>			// For file stream classes
+# include <iomanip>			// For input/output manipulation
+# include <exception>		// For standard exception classes
+# include <stdexcept>		// For standard exception classes (runtime_error, etc.)
+# include <limits>			// For numeric limits
+# include <typeinfo>		// For type information
+# include <vector>			// For using the vector container
+# include <list>	 		// For using the list container
+# include <map>		 		// For using the map container
+# include <algorithm>		// For using algorithms like sort, find, etc.
+# include <deque>	 		// For deque
+# include <stack>	 		// For stack
+# include <random>			// For creating random numbers
+# include <ctime>			// For time measurement
+
+# define RED	"\x1b[31;1m"
+# define GREEN	"\x1b[32;1m"
+# define BLUE	"\x1b[34;1m"
+# define CYAN	"\x1b[36;1m"
+# define YELLOW	"\033[33;1m"
+# define RESET	"\x1b[0m"
+
+#endif
